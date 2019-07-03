41d78b6e22965451e7d616a16b3905850c27e20e
Amal Elgalant <amalelgalant@amals-macbook-pro.local>
2019-07-03 17:01:59 +0200

    Initial Commit

---
+++ Whatchit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whatchit
+//
+//  Created by Amal Elgalant on 7/3/19.
+//  Copyright © 2019 Amal Elgalant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
